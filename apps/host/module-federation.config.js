93d5bc24bfb87113dd32ddb16014bbc4a695e6a5
Vevl <veverka.lubos@gmail.com>
2022-05-20 15:29:21 +0200

    add extra config

--- apps/host/module-federation.config.js
+++ apps/host/module-federation.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   name: 'host',
   remotes: ['test'],
+  shared: (libraryName, libraryOptions) => {
+    if (libraryName === '@angular/localize/init') {
+      return {
+        ...libraryOptions,
+        eager: true,
+      };
+    }
+  },
 };
